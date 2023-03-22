ef36797b390c8916033615787403199aa7af9b8c
MaksimLisovoi <lisovoi123@gmail.com>
2023-03-22 15:52:34 +0200

    ++

--- app.js
+++ app.js
@@ -8,23 +8,26 @@ require("dotenv").config();
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(boolParser());
 
 app.use("/", require("./routes/api"));
 
-// app.use(cors(), function (req, res, next) {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://wallet-app-project-nn33mm.netlify.app/"
-//   ); // update to match the domain you will make the request from//
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(cors(), function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://wallet-app-project-nn33mm.netlify.app/"
+  ); // update to match the domain you will make the request from//
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// res.setHeader("Access-Control-Allow-Origin", "*");
+// res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept");
 
 app.use((req, res) => {
   res
