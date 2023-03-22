9df9e8b563a2b8b85b55daa380ac981ce6c449e2
MaksimLisovoi <lisovoi123@gmail.com>
2023-03-22 13:58:05 +0200

    ++

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use("/", require("./routes/api"));
 app.use(cors(), function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://wallet-team-project.herokuapp.com"
-  );
+    "https://wallet-app-project-nn33mm.netlify.app/"
+  ); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
