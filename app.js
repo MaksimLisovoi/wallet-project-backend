75a276cb8d3ef8c31a40f0d6b4cbfa5af305930b
MaksimLisovoi <lisovoi123@gmail.com>
2023-03-22 16:31:14 +0200

    ++ cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://wallet-app-project-nn33mm.netlify.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
