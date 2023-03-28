eebc570f565cdfc41c6ec1f361e56b0bc7d5df15
MaksimLisovoi <lisovoi123@gmail.com>
2023-03-28 14:34:33 +0300

    + cors

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ const app = express();
 
 // app.use(cors(corsOptions));
 
-app.use(cors(), function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://api.privatbank.ua/",
-    "http://localhost:3000"
-  ); // update to match the domain you will make the request from//
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(cors(), function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://api.privatbank.ua/",
+//     "http://localhost:3000"
+//   ); // update to match the domain you will make the request from//
+//   res.header("Access-Control-Allow-Headers", "*");
+//   res.header("Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(helmet());
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(boolParser());
 
